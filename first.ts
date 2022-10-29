4c9654ea1f996ef870b4e6b106d176da82db24b0
whynotwhywhywhywhy <naomi981104@gmail.com>
2022-10-29 17:02:56 +0900

    기본 설정

---
+++ first.ts
@@ -0,0 +1,9 @@
+let Lyn: string = "바보"
+
+// TS에서 가장 중요한 파일 1. package.json 2. tsconfig.json
+//  설치 순서
+// 1. npm init -y  
+// 2. npm i typescript
+// 3.  npx tsc 자바스크립트로 변환
+// 4. npx tsc --init   tsconfig.json 파일 생성
+// 5. npx tsc --noEmit 타입 확인
